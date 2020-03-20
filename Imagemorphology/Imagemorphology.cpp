e39672b2551a22ed417d867cdb8f78496595d0c3
2077946749 <2077946749@qq.com>
2020-03-20 15:14:05 +0800

    2

--- Imagemorphology/Imagemorphology.cpp
+++ Imagemorphology/Imagemorphology.cpp
@@ -7,25 +7,27 @@ using namespace cv;
 
 int main()
 {
-	Mat srcMat = imread("coin.png",0);
-	cv::Mat binaryMat;
-	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
 
-	Mat elementSizel = getStructuringElement(MORPH_ELLIPSE, Size(9, 11));
-	Mat out;
-	Mat oout;
-	Mat om;
-	Mat cm;
-	dilate(binaryMat, out, elementSizel);
-	imshow("pengzhang", out);
-	erode(binaryMat, oout, elementSizel);
-	imshow("fushi", out);
-	morphologyEx(binaryMat, om, MORPH_OPEN, elementSizel);
-	imshow("binaryMat", binaryMat);
-	morphologyEx(binaryMat, cm, MORPH_CLOSE, elementSizel);
-	imshow("binaryMat", binaryMat);
-	waitKey(0);
-	return 0;
+		Mat srcMat = imread("coin.png",0);
+		Mat binaryMat;
+		Mat em, dm, om, cm;
+
+		threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+		Mat element = getStructuringElement(MORPH_RECT, Size(3,3));
+
+		erode(binaryMat, em, element);
+		dilate(binaryMat, dm, element);
+		morphologyEx(binaryMat, om, MORPH_OPEN, element);
+		morphologyEx(binaryMat, cm, MORPH_CLOSE, element);
+
+		imshow("frame", srcMat);
+		imshow("binaryMat", binaryMat);
+		imshow("erode", em);
+		imshow("dilate", dm);
+		imshow("opening", om);
+		imshow("closing", cm);
+
+		waitKey(0);
 
 }
 
--- two/two.cpp
+++ two/two.cpp
@@ -1,1 +1,49 @@
-﻿
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cv::Mat srcMat = imread("coin.png",0);
+	Mat binaryMat;
+	Mat labelMat;
+	Mat statsMat;
+	Mat centrMat;
+	Mat resultMat;
+
+	threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+	//获得连通域
+	int nComp = connectedComponentsWithStats(binaryMat, labelMat, statsMat, centrMat, 8, CV_32S);
+	//输出连通域信息
+	for (int i = 0; i < nComp; i++)
+	{//信息保存在statsMat
+		cout << "connected Components NO. " << i << endl;
+		cout << "pixels = " << statsMat.at<int>(i, 4) << endl;
+		cout << "width = " << statsMat.at<int>(i, 2) << endl;
+		cout << "height = " << statsMat.at<int>(i, 3) << endl;
+		cout << endl;
+	}
+	//包括背景
+	cout << "the munber is = " << nComp - 1 << endl;  //exclude background
+	//绘制
+	for (int i = 1; i < nComp; i++)
+	{
+		Rect bndbx;
+
+		bndbx.x = statsMat.at<int>(i, 0);
+		bndbx.y = statsMat.at<int>(i, 1);
+
+		bndbx.width = statsMat.at<int>(i, 2);
+		bndbx.height = statsMat.at<int>(i, 3);
+
+		rectangle(binaryMat, bndbx, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+ 
+	imshow("binaryMat", binaryMat);
+	imshow("frame", srcMat);
+	imwrite("results", binaryMat);
+	waitKey(0);
+	return 0;
+}
